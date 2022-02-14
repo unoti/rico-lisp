d8fb4da03700d3c6502a849dd823a3c9fb345e8a
unoti <unoti2@gmail.com>
2022-02-14 14:19:43 -0800

    added ch1

---
+++ ch1/README.md
@@ -0,0 +1,14 @@
+Goal of this section is to write an intepreter that can execute this program,
+where the program is expressed as a nested Python list.
+```py
+prg = [
+    ['print', 'my name is', 'richard'],
+    ['print', ['add', 1, 2]]
+]
+```
+
+The output of this program should be
+```
+my name is richard
+3
+```
---
+++ ch1/simpleprg.py
@@ -0,0 +1,41 @@
+from typing import List
+def main1():
+    prg = [['+', 3, 4]]
+    result = run(prg)
+    print(f'Program ended. Result={result}')
+
+def main2():   
+    prg = [ 
+        ['print', 'my name is', 'richard'],
+        ['print', ['+', 1, 2]]
+    ]
+    run(prg)
+
+
+def run(expressions: List):
+    """Run a sequence of commands, or evaluate a sequence of expressions."""
+    result = None
+    for expression in expressions:
+        result = eval(expression)
+    return result
+
+def eval(x):
+    if not isinstance(x, list):      # constant number or string
+        return x
+    operator = x[0]
+    rest = x[1:]
+
+    if operator == 'print':
+        items = [str(eval(item)) for item in rest]
+        output = ' '.join(items)
+        print(output)
+        return
+    
+    if operator == '+':
+        return rest[0] + rest[1]
+
+    raise Exception(f'Unknown operator {operator}')
+
+if __name__ == '__main__':
+    main1()
+    #main2()
